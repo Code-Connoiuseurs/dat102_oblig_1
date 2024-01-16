1fdf4d185a1ba5983896a6838391083f82218f30
Johan <142352311+h180240@users.noreply.github.com>
2024-01-16 13:39:14 +0100

    filmarkiv start

--- src/no/hvl/data102/filmarkiv/impl/Film.java
+++ src/no/hvl/data102/filmarkiv/impl/Film.java
@@ -1,5 +1,95 @@
 package no.hvl.data102.filmarkiv.impl;
 
+import java.util.Objects;
+
 public class Film {
 
+	private int filmnr;
+	private String produsent; //regissør
+	private String tittel;
+	private int aar; //lanseringsår
+	private Sjanger sjanger; //MÅ ENDRES PÅ. ENUM
+	private String filmselskap;
+	
+	public Film() {
+	}
+
+	public Film(int filmnr, String produsent, String tittel, int aar, Sjanger sjanger, String filmselskap) {
+		this.filmnr = filmnr;
+		this.produsent = produsent;
+		this.tittel = tittel;
+		this.aar = aar;
+		this.sjanger = sjanger;
+		this.filmselskap = filmselskap;
+	}
+
+	public int getFilmnr() {
+		return filmnr;
+	}
+
+	public void setFilmnr(int filmnr) {
+		this.filmnr = filmnr;
+	}
+
+	public String getProdusent() {
+		return produsent;
+	}
+
+	public void setProdusent(String produsent) {
+		this.produsent = produsent;
+	}
+
+	public String getTittel() {
+		return tittel;
+	}
+
+	public void setTittel(String tittel) {
+		this.tittel = tittel;
+	}
+
+	public int getAar() {
+		return aar;
+	}
+
+	public void setAar(int aar) {
+		this.aar = aar;
+	}
+
+	public Sjanger getSjanger() {
+		return sjanger;
+	}
+
+	public void setSjanger(Sjanger sjanger) {
+		this.sjanger = sjanger;
+	}
+
+	public String getFilmselskap() {
+		return filmselskap;
+	}
+
+	public void setFilmselskap(String filmselskap) {
+		this.filmselskap = filmselskap;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(aar, filmnr, filmselskap, produsent, sjanger, tittel);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Film other = (Film) obj;
+		return aar == other.aar && filmnr == other.filmnr && Objects.equals(filmselskap, other.filmselskap)
+				&& Objects.equals(produsent, other.produsent) && sjanger == other.sjanger
+				&& Objects.equals(tittel, other.tittel);
+	}
+	
+	
+	
 }
--- src/no/hvl/data102/filmarkiv/impl/Filmarkiv.java
+++ src/no/hvl/data102/filmarkiv/impl/Filmarkiv.java
@@ -1,5 +1,131 @@
 package no.hvl.data102.filmarkiv.impl;
 
-public class Filmarkiv {
+import java.util.Arrays;
 
+import no.hvl.data102.filmarkiv.adt.FilmarkivADT;
+
+public class Filmarkiv implements FilmarkivADT {
+
+	private Film[] filmTab;
+	private int antall;
+
+	public Filmarkiv(int antall) {
+		this.filmTab = new Film[antall];
+		this.antall = 0;
+	}
+
+	@Override
+	public Film finnFilm(int nr) {
+		for (Film film : filmTab) {
+			if (nr == film.getFilmnr()) {
+				return film;
+			}
+		}
+		System.out.println("Film ikke funnet.");
+		return null;
+	}
+
+//		int i = 0;
+//		while (i < filmTab.length) {
+//			if (nr == filmTab[i].getFilmnr()) {
+//				return filmTab[i];
+//			} else {
+//				i++;
+//			}
+//		}
+//		System.out.println("Film ikke funnet.");
+//		return null;
+//	}
+
+	@Override
+	public void leggTilFilm(Film nyFilm) {
+		if (antall == filmTab.length) {
+			filmTab = Arrays.copyOf(filmTab, filmTab.length * 2);
+		}
+		filmTab[antall] = nyFilm;
+		antall++;
+	}
+
+	@Override
+	public boolean slettFilm(int filmnr) {
+		boolean funnet = false;
+		int indeks = 0;
+		while (indeks < antall && !funnet) {
+			if (filmnr == filmTab[indeks].getFilmnr()) {
+				funnet = true;
+			} else {
+				indeks++;
+			}
+		}
+		if (!funnet) {
+			return false;
+		}
+
+		filmTab[indeks] = filmTab[antall - 1];
+		filmTab[antall - 1] = null;
+		antall--;
+		return true;
+	}
+
+	@Override
+	public Film[] soekTittel(String delstreng) {
+
+		Film[] soekTab = new Film[filmTab.length];
+		int i = 0;
+
+		for (Film film : filmTab) {
+			if (film.getTittel().contains(delstreng)) {
+				soekTab[i] = film;
+				i++;
+			}
+			soekTab = trimTab(soekTab, i);
+			return soekTab;
+		}
+		System.out.println("Ingen filmer funnet.");
+		return null;
+	}
+
+	@Override
+	public Film[] soekProdusent(String delstreng) {
+		Film[] soekTab = new Film[filmTab.length];
+		int i = 0;
+
+		for (Film film : filmTab) {
+			if (film.getProdusent().contains(delstreng)) {
+				soekTab[i] = film;
+				i++;
+			}
+			soekTab = trimTab(soekTab, i);
+			return soekTab;
+		}
+		System.out.println("Ingen filmer funnet.");
+		return null;
+	}
+
+	@Override
+	public int antall(Sjanger sjanger) {
+		int i = 0;
+		for (Film film : filmTab) {
+			if (film.getSjanger() == sjanger) {
+				i++;
+			}
+		}
+		return i;
+	}
+
+	@Override
+	public int antall() {
+		return antall;
+	}
+
+	private Film[] trimTab(Film[] tab, int n) {
+		// n er antall elementer
+		Film[] nytab = new Film[n];
+		int i = 0;
+		while (i < n) {
+			nytab[i] = tab[i];
+			i++;
+		}
+		return nytab;
+	}
 }
